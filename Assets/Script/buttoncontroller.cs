4c0214f1848c5f3ab910e588cc60b981b0cc7724
KimHyunMin <68584900+koenig2442@users.noreply.github.com>
2022-10-20 07:40:39 +0900

    씬이동 구현

---
+++ Assets/Script/buttoncontroller.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttoncontroller : MonoBehaviour
+{
+ public void Charactors_down()
+    {
+        SceneManager.LoadScene("HeroMenu");
+    }
+    public void StageSelect_down()
+    {
+        SceneManager.LoadScene("StageSelect");
+    }
+    public void Charactors_Party_down()
+    {
+        SceneManager.LoadScene("UI_Party");
+    }
+    public void button_200_inactive_down()
+    {
+        SceneManager.LoadScene("InGame_UI");
+    }
+    public void Backspace()
+    {
+        SceneManager.LoadScene("MainLobby");
+    }
+}
+
