477cf9d546fece96eab26e4cc77ae7ac89f1a024
rlaalsxo <alsxo124@naver.com>
2022-12-02 20:21:38 +0900

    캐릭터 강화창 업데이트

--- Assets/Script/ManagerWindow.cs
+++ Assets/Script/ManagerWindow.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class ManagerWindow : MonoBehaviour, IPointerDownHandler
+public class ManagerWindow : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
 {
     GameObject StatusWindow;
     GameObject HeroWindow;
     GameObject WeaponWindow;
     GameObject LevelBar;
+    GameObject HeroLevel;
+    public GameObject selectedSlot;
+    string herolevel;
+    int level = 1;
     string gold;
     int SpentGold;
     string useGold;
@@ -19,6 +23,8 @@ public class ManagerWindow : MonoBehaviour, IPointerDownHandler
         HeroWindow = GameObject.Find("HeroWindow");
         WeaponWindow = GameObject.Find("WeaponWindow");
         LevelBar = GameObject.Find("LevelBar");
+        HeroLevel = GameObject.Find("HeroLevel");
+        herolevel = HeroLevel.GetComponent<Text>().text;
         gold = GameObject.Find("Cash").GetComponent<Text>().text;
         useGold = GameObject.Find("Spent").GetComponent<Text>().text;
     }
@@ -88,24 +94,65 @@ public class ManagerWindow : MonoBehaviour, IPointerDownHandler
                 WeaponesOpen();
             }
         }
+        if(ui.name == "HeroSlot")
+        {
+            if(ui.GetComponent<Image>().sprite != null)
+            {
+                selectedSlot.GetComponent<Image>().sprite = ui.GetComponent<Image>().sprite;
+                selectedSlot.SetActive(true);
+                selectedSlot.transform.position = _eventData.position;
+            } 
+        }
+    }
+    public void OnPointerUp(PointerEventData _eventData)
+    {
+        GameObject ui = _eventData.pointerEnter;
+        Debug.Log(ui.name);
+        if (selectedSlot.activeSelf == true)
+        {
+            if(ui.name == "Character")
+            {
+                ui.GetComponent<Image>().sprite = selectedSlot.GetComponent<Image>().sprite;
+            }
+            selectedSlot.SetActive(false);
+        }
+        
+    }
+    public void OnDrag(PointerEventData _eventData)
+    {
+        if(selectedSlot.activeSelf == true)
+        {
+            selectedSlot.transform.position = _eventData.position;
+        }
     }
     public void LevelUp()
     {
         SpentGold = int.Parse(useGold);
         int cash = int.Parse(gold);
-        cash -= SpentGold;
-        SpentGold += 100;
-        useGold = SpentGold.ToString();
-        gold = cash.ToString();
-        GameObject.Find("Cash").GetComponent<Text>().text = gold;
-        GameObject.Find("Spent").GetComponent<Text>().text = useGold;
-        float exp = LevelBar.GetComponent<Image>().fillAmount;
-        exp += 0.8f;
-        LevelBar.GetComponent<Image>().fillAmount = exp;
-        if (exp >= 1f)
+        if(cash >= SpentGold)
         {
-            exp -= 1f;
+            cash -= SpentGold;
+            SpentGold += 100;
+            useGold = SpentGold.ToString();
+            gold = cash.ToString();
+            GameObject.Find("Cash").GetComponent<Text>().text = gold;
+            GameObject.Find("Spent").GetComponent<Text>().text = useGold;
+            float exp = LevelBar.GetComponent<Image>().fillAmount;
+            exp += 0.8f;
             LevelBar.GetComponent<Image>().fillAmount = exp;
+            if (exp >= 1f)
+            {
+                exp -= 1f;
+                level++;
+                herolevel = "Lv." + level;
+                HeroLevel.GetComponent<Text>().text = herolevel;
+                LevelBar.GetComponent<Image>().fillAmount = exp;
+                
+            }
+        }
+        else if(cash < SpentGold)
+        {
+            Debug.Log("돈이 부족합니다");
         }
     }
 }
