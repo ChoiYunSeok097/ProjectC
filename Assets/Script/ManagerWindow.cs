83a4d279d6519d931ff544332bd4746892e67dee
rlaalsxo <alsxo124@naver.com>
2022-12-02 23:34:25 +0900

    캐릭터 강화창 업데이트2

--- Assets/Script/ManagerWindow.cs
+++ Assets/Script/ManagerWindow.cs
@@ -6,12 +6,44 @@ using UnityEngine.EventSystems;
 
 public class ManagerWindow : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
 {
+    Character stat;
+    public void setScript(string _name)
+    {
+        // character's stat is loaded from file
+        List<string> str = _Data_DataInput.instance.loadFile("/userCharacter.csv");
+
+        foreach (string one in str)
+        {
+            string[] contents = one.Split(',');
+
+            //find character in File
+            if (_name == contents[0])
+            {
+                stat = new Character();
+                // input character state
+                stat.Level = level;
+                stat.hp = float.Parse(contents[1]) + level * 2;
+                stat.armor = float.Parse(contents[2]) + level * 0.2f;
+                stat.attack = float.Parse(contents[3]) + level * 0.5f;
+                stat.attackSpeed = float.Parse(contents[4]);
+                stat.attackRange = float.Parse(contents[5]);
+                stat.speed = float.Parse(contents[6]);
+                stat.job = float.Parse(contents[7]);
+            }
+
+        }
+    }
     GameObject StatusWindow;
     GameObject HeroWindow;
     GameObject WeaponWindow;
     GameObject LevelBar;
     GameObject HeroLevel;
     public GameObject selectedSlot;
+    public Text HeroName;
+    public Image CharacterSlot;
+    public Text Hpstat;
+    public Text AttackStat;
+    public Text DefStat;
     string herolevel;
     int level = 1;
     string gold;
@@ -34,7 +66,6 @@ public class ManagerWindow : MonoBehaviour, IPointerDownHandler, IPointerUpHandl
         StatusWindow.SetActive(false);
         HeroWindow.SetActive(true);
         WeaponWindow.SetActive(false);
-
     }
 
     // Update is called once per frame
@@ -113,6 +144,7 @@ public class ManagerWindow : MonoBehaviour, IPointerDownHandler, IPointerUpHandl
             if(ui.name == "Character")
             {
                 ui.GetComponent<Image>().sprite = selectedSlot.GetComponent<Image>().sprite;
+                StatUpdate();
             }
             selectedSlot.SetActive(false);
         }
@@ -147,12 +179,24 @@ public class ManagerWindow : MonoBehaviour, IPointerDownHandler, IPointerUpHandl
                 herolevel = "Lv." + level;
                 HeroLevel.GetComponent<Text>().text = herolevel;
                 LevelBar.GetComponent<Image>().fillAmount = exp;
-                
+                StatUpdate();
+                Debug.Log(level);
             }
         }
         else if(cash < SpentGold)
         {
             Debug.Log("돈이 부족합니다");
+        }
+    }
+    void StatUpdate()
+    {
+        if (CharacterSlot.sprite.name != "SelectedHero" && CharacterSlot.sprite != null)
+        {
+            setScript(CharacterSlot.sprite.name);
+            Hpstat.text = "체력 : " + stat.hp.ToString();
+            AttackStat.text = "공격력 : " + stat.attack.ToString();
+            DefStat.text = "방어력 : " + stat.armor.ToString();
+            HeroName.text = CharacterSlot.sprite.name;
         }
     }
 }
